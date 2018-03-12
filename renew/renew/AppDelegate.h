11d8bcf3d80d2404f949b53adae17e8c1da1ccd3
toureek <june.key.young@gmail.com>
2018-03-12 20:55:22 +0800

    Init iOS Project

---
+++ renew/renew/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  renew
+//
+//  Created by younghacker on 3/12/18.
+//  Copyright © 2018 toureek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ renew/renew/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  renew
+//
+//  Created by younghacker on 3/12/18.
+//  Copyright © 2018 toureek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
